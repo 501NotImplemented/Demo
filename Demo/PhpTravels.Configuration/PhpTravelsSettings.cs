c1c773f78d3313fb08bf79346ad39bd180793811
501NotImplemented <32591300+501NotImplemented@users.noreply.github.com>
2020-09-13 12:26:08 +0300

    Extract configuration (#5)
    * Extact PhpTravels.Configuration
    Move config values to appsettings.json

---
+++ Demo/PhpTravels.Configuration/PhpTravelsSettings.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PhpTravels.Configuration
+{
+	public class PhpTravelsSettings
+	{
+		private IConfigurationRoot _configuration;
+
+		internal PhpTravelsSettings()
+		{
+			var serviceCollection = new ServiceCollection();
+			ConfigureServices(serviceCollection);
+		}
+
+		public string AdminPassword
+		{
+			get
+			{
+				var initialValue = _configuration.GetSection("AdminSettings:Password").Value;
+				var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(initialValue));
+				return decoded;
+			}
+		}
+
+		public string AdminUserName
+		{
+			get
+			{
+				var initialValue = _configuration.GetSection("AdminSettings:Username").Value;
+				var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(initialValue));
+				return decoded;
+			}
+		}
+
+		public string BaseUrl => _configuration["BaseUrl"];
+
+		public string DemoUserName
+		{
+			get
+			{
+				var initialValue = _configuration.GetSection("UserSettings:Username").Value;
+				var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(initialValue));
+				return decoded;
+			}
+		}
+
+		public string DemoUserPassword
+		{
+			get
+			{
+				var initialValue = _configuration.GetSection("UserSettings:Password").Value;
+				var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(initialValue));
+				return decoded;
+			}
+		}
+
+		private void ConfigureServices(IServiceCollection serviceCollection)
+		{
+			_configuration = new ConfigurationBuilder()
+							.SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+							.AddJsonFile("appsettings.json", false)
+							.Build();
+
+			serviceCollection.AddSingleton(_configuration);
+		}
+	}
+}
